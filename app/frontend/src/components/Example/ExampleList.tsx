7e94a0c2787cbccdef850fe216b57ae625d25291
Kevin Boucher <kboucher@gmail.com>
2024-07-10 19:03:44 +0000

    f

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -3,9 +3,9 @@ import { Example } from "./Example";
 import styles from "./Example.module.css";
 
 const DEFAULT_EXAMPLES: string[] = [
-    "What is included in my Northwind Health Plus plan that is not in standard?",
-    "What happens in a performance review?",
-    "What does a Product Manager do?"
+    "How many Security Requirements are there in Level 2?",
+    "How many CMMC domains are there?",
+    "What is the CMMC Assessment Guide?"
 ];
 
 const GPT4V_EXAMPLES: string[] = [
